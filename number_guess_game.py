b206a914afbb343e4f39862c4cf8ed8a8916bd76
siewilliam01 <siewilliam01@gmail.com>
2025-01-28 10:20:13 -0800

    Update number_guess_game.py

--- number_guess_game.py
+++ number_guess_game.py
@@ -3,6 +3,7 @@ import random
 def main():
     random_number = random.randint(0, 9)
     win = False
+    count = 1
     while(win == False):
         user_number = input("Guess my number from 0-9 >>> ")
         try:
@@ -11,10 +12,15 @@ def main():
             print("'", user_number, "'", "is not an acceptable input. Try again.")
         else:
             if(user_number == random_number):
-                print("Good guess! My number was indeed", random_number, ". New Game!")
+                print("Good guess! My number was indeed", random_number, ". You guessed in", count, "tries. New Game!")
                 win = True
-            else:
-                print("Wrong! Try Again!")
+            elif(user_number <= random_number):
+                print("Too small! Try again!")
+                count += 1
+            elif(user_number >= random_number):
+                print("Too big! Try again!")
+                count += 1
+                
 
 while True:
     main()
