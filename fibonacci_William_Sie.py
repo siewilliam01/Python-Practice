884251e0a9219cf31c182c5803d3ac29cd62667a
siewilliam01 <siewilliam01@gmail.com>
2025-02-04 08:06:46 -0800

    Update fibonacci_William_Sie.py

--- fibonacci_William_Sie.py
+++ fibonacci_William_Sie.py
@@ -1,14 +1,54 @@
 import time
+import random
 
-fib_seq = [0, 1]
+my_fib = []
+fib_int1 = 0
+fib_int2 = 0
 
-def main():
-    fib_next = fib_seq[-1] + fib_seq[-2]
-    fib_seq.append(fib_next)
-    time.sleep(.1)
+def startup():
+    global my_fib
+    global fib_int1
+    global fib_int2
+    my_fib = []
+    fib_int1 = 0
+    fib_int2 = 0
+
+def ask():
+    global fib_int1
+    input_correct = False
+    while(input_correct == False):
+        fib_int1 = input("Input an integer from 0-9 >>> ")
+        try:
+            fib_int1 = int(fib_int1)
+            if(fib_int1 > 9 or fib_int1 < 0):
+                raise
+        except:
+            print(fib_int1, "is not an integer from 0-9, try again.")
+        else:
+            input_correct = True
+    print("You have input", fib_int1, "as your integer.")
+    
+def random_generator():
+    global fib_int2
+    fib_int2 = random.randint(1, 20)
+    print("The computer has randomly chosen", fib_int2)
+    
+def start_sequence():
+    my_fib.append(fib_int1)
+    my_fib.append(fib_int2)
+    print(my_fib)
+    
+def repeat_sequence():
+    for i in range(10):
+        time.sleep(0.1)
+        my_fib.append(my_fib[-1] + my_fib[-2])
+        print(my_fib)
 
 while True:
-    for i in range(10):
-        print(fib_seq[-1])
-        main()
-    time.sleep(1)
+    startup()
+    ask()
+    random_generator()
+    start_sequence()
+    repeat_sequence()
+    print(my_fib, "Done.")
+
